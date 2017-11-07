12655b523bd33f52ff400ee210e2a6890e22201c
Jj <jejeabdulrahman@gmail.com>
2017-11-07 14:48:33 +0700

    first skypebot for master

--- app.js
+++ app.js
@@ -1,38 +1,113 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var request = require('request');
 //=========================================================
 // Bot Setup
 //=========================================================
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 8080, function () {
+server.use(restify.plugins.queryParser({
+  mapParams: true
+}));
+server.use(restify.plugins.bodyParser({
+  mapParams: true
+}));
+
+server.listen(process.env.port || process.env.PORT || 3000, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
-// Create chat bot
+
 var connector = new builder.ChatConnector({
   appId: "5227171c-6acb-4aaa-9a14-4e19736fb32c",
   appPassword: "FgL8oVJpo9ukazazuwXwjrM"
 });
 var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-//Bot on
-bot.on('contactRelationUpdate', function (message) {
-  if (message.action === 'add') {
-    var name = message.user ? message.user.name : null;
-    var reply = new builder.Message()
-      .address(message.address)
-      .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
-    bot.send(reply);
-  } else {
-    // delete their data
+server.post('/repos/:owner/:repo/hooks', (req, res, next) => {
+
+  if (!req.is('application/json')) {
+    return next(
+      new errors.InvalidContentError("Expects 'application/json'")
+    );
   }
-});
-bot.on('typing', function (message) {
-  // User is typing
-});
-bot.on('deleteUserData', function (message) {
-  // User asked to delete their data
-});
+
+  // Process posted notification
+  var address =
+    {
+      id: '1505191202170',
+      channelId: 'skype',
+      user:
+      {
+        id: '29:1MChe3Vp8MCNVJKL1A3FtCvAhiCfp1I2U0BbxggCboZs',
+        name: 'Jeje Abdul Rahman',
+      },
+      conversation:
+      {
+        isGroup: true,
+        id: '19:89d940f123144dda8e87a916d62ff2c1@thread.skype',
+        // id: '19:I2RtYXN0YWcvJDc1Yzg4ZWI0NzhjZjMyNGY=@p2p.thread.skype',
+      },
+      bot:
+      {
+        id: '28:5227171c-6acb-4aaa-9a14-4e19736fb32c',
+        name: 'goBot',
+      },
+      serviceUrl: 'https://smba.trafficmanager.net/apis/',
+      useAuth: true
+    };
+  // var address = JSON.parse(req.body.address);
+  // var notification = req.body.notification;
+
+  let data = req.body || {};
+
+  if (data['project']) {
+    // Send notification as a proactive message
+    if (data.object_kind == 'push') {
+      console.log(data, 'Hook okeey========<> push');
+      var msg = new builder.Message()
+        .address(address)
+        .text(" Hi... I'm gobot. :D \n\n Event Name : " + data['event_name'] + '\n\n UserName : ' + data['user_name'] + '\n\n Project : ' + data['project']['name'] + '\n\n Default Branch : ' + data['project']['default_branch'] + '\n\n Description : ' + data.commits[0]['message'] + '\n\n Timestamp : ' + data.commits[0]['timestamp']);
+
+    } else if (data.object_kind == 'pipeline') {
+      console.log(data['builds'], '=JJ=');
+      var msg = new builder.Message()
+        .address(address)
+        .text(" Ref : " + data['object_attributes']['ref']
+        + "\n\n Project : " + data['project']['name']
+        + "\n\n Author : " + data['commit']['author']['name']
+        + "\n\n Name : " + data['builds'][1]['name']
+        + "\n\n==\n\n Build : " + data['builds'][1]['name']
+        + "\n\n User : " + data['builds'][1]['user']['name']
+        + "\n\n File Name : " + data['builds'][1]['artifacts_file']['filename']
+        + "\n\n Size : " + data['builds'][1]['artifacts_file']['size']
+        + "\n\n==\n\n Build : " + data['builds'][2]['name']
+        + "\n\n User : " + data['builds'][2]['user']['name']
+        + "\n\n File Name : " + data['builds'][2]['artifacts_file']['filename']
+        + "\n\n Size : " + data['builds'][2]['artifacts_file']['size']
+        + "\n\n==\n\n Build : " + data['builds'][3]['name']
+        + "\n\n User : " + data['builds'][3]['user']['name']
+        + "\n\n File Name : " + data['builds'][3]['artifacts_file']['filename']
+        + "\n\n Size : " + data['builds'][3]['artifacts_file']['size']);
+    } else {
+      var msg = new builder.Message()
+        .address(address)
+        .text("Sorry your request could not be processed. please try again :( ");
+    }
+
+    // =======================
+    request('http://192.168.0.97:3000/api/versions', function (error, response, body) {
+      console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+
+    bot.send(msg, function (err) {
+      console.log('Bot success send message');
+    });
+
+  }
+
+  res.send(200);
+  return next();
+}, connector.listen());
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
@@ -41,25 +116,22 @@ String.prototype.contains = function (content) {
 }
 
 bot.dialog('/', function (session) {
-  console.log('this is wew');
-  if (session.message.text.toLowerCase().contains('hello')) {
-    session.send(`Hey, How are you?`);
-  } else if (session.message.text.toLowerCase().contains('help')) {
-    session.send(`How can I help you?`);
-  } else if (session.message.text.toLowerCase().contains('nama')) {
-    session.send(`Nama kamu siapa?`);
-  } else if (session.message.text.toLowerCase().contains('apa')) {
-    session.send(`Ada yang bisa saya bantu?`);
-  } else if (session.message.text.toLowerCase().contains('game')) {
-    session.send(`Sevent paladins aja, seru broow?`);
-  } else {
-    session.send(`Chatingan mulu, kerja kerja kerja .....`);
-  }
+  console.log(session, 'this is wew');
+  session.sendTyping();
+  setTimeout(function () {
+    if (session.message.text.toLowerCase().contains('hello')) {
+      session.send(`Hey, How are you?`);
+    } else if (session.message.text.toLowerCase().contains('help')) {
+      session.send(`How can I help you?`);
+    } else if (session.message.text.toLowerCase().contains('sholat')) {
+      session.send(`Hayu kita sholat awal waktu guys (mm)`);
+    } else {
+      session.send(`Sorry I don't understand you... :(`);
+    }
+  }, 3000);
 });
 
-/**
-	 * LIST
-	 */
-server.get('/repos/:owner/:repo/hooks', (req, res, next) => {
-  console.log(req,'Hook okeey',res);
+// pipeline
+server.post('/pipeline/hooks', (req, res, next) => {
+  console.log('aaaak');
 });
